680d11cd1839196a85e1de5c715bf44957d9c232
khushiasinfotech-oss <khushiasinfotech@gmail.com>
2026-05-16 14:14:52 +0530

    Trigger redeploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Deployment trigger comment
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
